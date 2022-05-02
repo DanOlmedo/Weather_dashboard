887304a2436b7c76846dbe8e31a8d1682ec76ea9
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-05-01 19:41:36 -0500

    Final additions for submitions

--- testscript.js
+++ testscript.js
@@ -24,9 +24,72 @@ function informationfunction() {
          currentTemperature.innerHTML = "Current temperature (ºC): " + temperatureValue;
          currentHumidity.innerHTML = "Current humidity (%): " + humidityValue;
          currentWind.innerHTML = "Current wind speed (m/s): " + windValue;
-        
+         localStorage.setItem('city',cityName);
+         addRecent();
 });
 
 }
 
 button.addEventListener('click', informationfunction);
+
+var recentSearches = [];
+var recent1 = document.querySelector('#recent1');
+var recent2 = document.querySelector('#recent2');
+var recent3 = document.querySelector('#recent3');
+
+function addRecent(){
+    recentSearches.unshift(cityName);
+    localStorage.setItem('testRecent',recentSearches);
+    console.log(recentSearches.slice(0,3));
+    recent1.innerHTML = recentSearches[0];
+    recent2.innerHTML = recentSearches[1];
+    recent3.innerHTML = recentSearches[2];
+}
+
+function getFromRecent2(){
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+recent2.innerHTML+'&appid=ae90054c5cfbcc338314940f67ed4e1e&units=metric')
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(
+        data => {
+        cityName = data['name'];
+         temperatureValue = data['main']['temp'];
+         humidityValue = data ['main']['humidity'];
+         weatherValue = data['weather']["0"]["main"];
+         windValue = data['wind']['speed'];
+         currentCity.innerHTML = "Current city: " + cityName;
+         currentWeather.innerHTML = "Current weather: " + weatherValue;
+         currentTemperature.innerHTML = "Current temperature (ºC): " + temperatureValue;
+         currentHumidity.innerHTML = "Current humidity (%): " + humidityValue;
+         currentWind.innerHTML = "Current wind speed (m/s): " + windValue;
+        }
+    )
+};
+
+function getFromRecent3(){
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+recent3.innerHTML+'&appid=ae90054c5cfbcc338314940f67ed4e1e&units=metric')
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(
+        data => {
+        cityName = data['name'];
+         temperatureValue = data['main']['temp'];
+         humidityValue = data ['main']['humidity'];
+         weatherValue = data['weather']["0"]["main"];
+         windValue = data['wind']['speed'];
+         currentCity.innerHTML = "Current city: " + cityName;
+         currentWeather.innerHTML = "Current weather: " + weatherValue;
+         currentTemperature.innerHTML = "Current temperature (ºC): " + temperatureValue;
+         currentHumidity.innerHTML = "Current humidity (%): " + humidityValue;
+         currentWind.innerHTML = "Current wind speed (m/s): " + windValue;
+        }
+    )
+};
+
+var recent2button = document.querySelector('#r2btn');
+var recent3button = document.querySelector('#r3btn');
+
+recent2button.addEventListener('click',getFromRecent2);
+recent3button.addEventListener('click',getFromRecent3);
+
+alert("Hello grader, I am sorry, I think I can not access the weekly functionality for the API because it requires payment");
